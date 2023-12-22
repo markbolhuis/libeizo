9536c950b29d621996e677cec5731765bc70f98f
Mark Bolhuis <mark@bolhuis.dev>
2023-12-22 15:11:30 +0100

    Use eizo_handle_t typedef

--- include/eizo/debug.h
+++ include/eizo/debug.h
@@ -9,7 +9,7 @@ void
 eizo_dbg_dump_ff300009(eizo_handle_t handle);
 
 void
-eizo_dbg_dump_custom_key_lock(struct eizo_handle *handle);
+eizo_dbg_dump_custom_key_lock(eizo_handle_t handle);
 
 int
 eizo_dbg_poll(struct eizo_handle *handle);
